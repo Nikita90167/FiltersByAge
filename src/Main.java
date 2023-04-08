b3fafdf54daa52898d527af34df78ba9a77dc0ff
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-04-08 16:05:09 +0300

    divided everything into methods

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
 
 public class Main {
+    public static Long filterByAge (Collection<Person> people, int ageFrom) {
+        return people.stream()
+                .filter(person -> person.getAge() >= ageFrom)
+                .count();
+    }
+    public static Collection<String> ageFilterForConscripts (Collection<Person> people, int ageFrom, int ageTo) {
+       return people.stream()
+                .filter(person -> person.getSex().equals(Sex.MAN))
+                .filter(person -> person.getAge() >= 18 && person.getAge() < 28)
+                .map(person -> String.valueOf(person.getFamily()))
+                .toList();
+    }
+    public static Collection<String> workablePerson (Collection<Person> people, int ageFrom, int ageToMan, int ageToWoman) {
+        return people.stream()
+                .filter(person -> person.getSex() == Sex.MAN ? person.getAge() > ageFrom && person.getAge() < ageToMan + 1 : person.getAge() > ageFrom && person.getAge() < ageToWoman + 1)
+                .filter(person -> person.getEducation().equals(Education.HIGHER))
+                .map(person -> String.valueOf(person.getFamily()))
+                .sorted(Comparator.naturalOrder())
+                .toList();
+    }
     public static void main(String[] args) {
        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
@@ -17,16 +33,6 @@ public class Main {
                    Sex.values()[new Random().nextInt(Sex.values().length)],
                    Education.values()[new Random().nextInt(Education.values().length)]));
         }
-       Long CountAgeNo18 = persons.stream()
-               .filter(personAge -> personAge.getAge() < 18)
-               .count();
-        List<String> surnames = persons.stream()
-                .filter(personSurname -> personSurname.getSex().equals(Sex.MAN))
-                .filter(person -> person.getAge() >= 18 && person.getAge() < 28)
-                .map(person -> String.valueOf(person.getFamily()))
-                .toList();
-        System.out.println(surnames);
-
 
     }
 }
